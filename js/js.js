faa00320462dee7d0d1c4c2905313bd0d0b81dba
4sTon <116713927+4sTon@users.noreply.github.com>
2023-01-17 13:41:03 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function verif(){
+    a=document.form.Nom.value;
+    b=document.form.Prénom.value;
+    c=document.form.Avis.value;
+    if(a==""||b==""||c==""){alert("remplisser tous les champs")}
+      
+}
